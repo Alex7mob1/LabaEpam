f32bf51f37318d44df5cce055cadca88125d2962
Admin <oles.mob1@gmail.com>
2021-07-23 11:20:14 +0300

    update

--- HW-2/src/test/java/rozetkaTest/BucketTests.java
+++ HW-2/src/test/java/rozetkaTest/BucketTests.java
@@ -1,21 +1,30 @@
 package rozetkaTest;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import javax.annotation.Nullable;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
+
+
+import static org.openqa.selenium.By.cssSelector;
+import static org.openqa.selenium.By.xpath;
+import static org.testng.Assert.assertEquals;
 
 public class BucketTests {
 
     private WebDriver driver;
-    private WebDriverWait wait;
+    private String expectedPrice = "125 988";
+    private WebDriver wait;
+
 
     @BeforeTest
     public void profileSetUp() {
@@ -32,52 +41,76 @@ public class BucketTests {
 
     @Test
     public void chooseProduct() {
-        WebElement webElement = driver.findElement(By.xpath("//*[@class='sidebar sidebar_type_main']//*[contains(@href, 'computers-notebooks')]"));
-        webElement.click();
-        webElement = driver.findElement(By.xpath("//*[@title='Компьютеры']"));
+
+        Actions actions = new Actions(driver);
+
+        driver.findElement(xpath("//*[@class='sidebar sidebar_type_main']//*[contains(@href, 'computers-notebooks')]")).click();
+
+        WebElement webElement = driver.findElement(By.xpath("//*[@title='Компьютеры']"));
         webElement.click();
         webElement = driver.findElement(By.xpath("//*[contains(@href, 'producer=asus')]"));
         webElement.click();
-        webElement = driver.findElement(By.xpath("//*[contains(text(),' От дорогих к дешевым ')]"));
+        webElement = driver.findElement(By.cssSelector("option[value='2: expensive']"));
         webElement.click();
-        webElement = driver.findElement(By.xpath("//*[@title='Компьютер Asus ROG Strix G35DX (90PD02W1-M12900)']"));
+        webElement = driver.findElement(By.cssSelector("ul.catalog-grid li:first-child span.goods-tile__title"));
         webElement.click();
-        webElement = driver.findElement(By.xpath("//*[@class='product-buttons__item ng-star-inserted']"));
+        webElement = driver.findElement(xpath("//*[@class='product-buttons__item ng-star-inserted']"));
+        actions.moveToElement(webElement).perform();
         webElement.click();
-        webElement = driver.findElement(By.xpath("//*[@style='visibility: visible;']"));
+        webElement = driver.findElement(xpath("//*[@class='button button_size_medium button_color_gray cart-footer__continue ng-star-inserted']"));
         webElement.click();
-        webElement = driver.findElement(By.xpath("//*[@class='breadcrumbs__icon-home']"));
+        webElement = driver.findElement(xpath("//*[@class='breadcrumbs__icon-home']"));
         webElement.click();
-        webElement = driver.findElement(By.xpath("//*[@class='sidebar sidebar_type_main']//*[contains(@href,'uvlecheniya')]"));
+        webElement = driver.findElement(xpath("//*[@class='sidebar sidebar_type_main']//*[contains(@href,'uvlecheniya')]"));
         webElement.click();
-        webElement = driver.findElement(By.xpath("//*[@title='Велосипеды']"));
+        webElement = driver.findElement(xpath("//*[@title='Велосипеды']"));
         webElement.click();
-        webElement = driver.findElement(By.cssSelector("label[for='GT']"));
-        webElement.click();
-        webElement = driver.findElement(By.xpath("//*[contains(text(),' От дорогих к дешевым ')]"));
+
+        new WebDriverWait(driver, 30)
+                .ignoring(StaleElementReferenceException.class)
+                .until((WebDriver d) -> {
+                    d.findElement(By.xpath("//*[@id='GT']/following-sibling::*")).isEnabled();
+                    return true;
+                });
+
+        WebElement element = driver.findElement(By.xpath("//*[@id='GT']/following-sibling::*"));
+
+        actions.moveToElement(element);
+
+        actions.perform();
+
+
+        driver.findElement(By.xpath("//*[@id='GT']/following-sibling::*")).click();
+        webElement = driver.findElement(xpath("//*[contains(text(),' От дорогих к дешевым ')]"));
         webElement.click();
         webElement = driver.findElement(By.cssSelector("a[title = 'Велосипед GT AVALANCHE 27 COMP 2020']"));
         webElement.click();
-        webElement = driver.findElement(By.xpath("//*[@class='product-buttons__item ng-star-inserted']"));
+        webElement = driver.findElement(xpath("//*[@class='product-buttons__item ng-star-inserted']"));
         webElement.click();
-        webElement = driver.findElement(By.xpath("//*[@style='visibility: visible;']"));
+        webElement = driver.findElement(xpath("//*[@class='button button_size_medium button_color_gray cart-footer__continue ng-star-inserted']"));
         webElement.click();
-        webElement = driver.findElement(By.xpath("//*[@class='breadcrumbs__icon-home']"));
+        webElement = driver.findElement(xpath("//*[@class='breadcrumbs__icon-home']"));
         webElement.click();
-        webElement = driver.findElement(By.xpath("//*[@class='sidebar sidebar_type_main']//*[contains(@href,'shoes')]"));
+        webElement = driver.findElement(xpath("//*[@class='sidebar sidebar_type_main']//*[contains(@href,'shoes')]"));
         webElement.click();
-        webElement = driver.findElement(By.xpath("//*[contains(text(),' Мужчинам ')]"));
+        webElement = driver.findElement(xpath("//*[contains(text(),' Мужчинам ')]"));
         webElement.click();
-        webElement = driver.findElement(By.xpath("//*[contains(text(),' Обувь')]"));
+        webElement = driver.findElement(xpath("//*[contains(text(),' Обувь')]"));
         webElement.click();
         webElement = driver.findElement(By.cssSelector("img[alt='Сандалии']"));
         webElement.click();
         webElement = driver.findElement(By.cssSelector("label[for='Jack Wolfskin']"));
         webElement.click();
-        webElement = driver.findElement(By.xpath("//*[contains(text(),' От дорогих к дешевым ')]"));
+        webElement = driver.findElement(xpath("//*[contains(text(),' От дорогих к дешевым ')]"));
         webElement.click();
-        webElement = driver.findElement(By.xpath("//*[contains(text(),'Сандалии Jack Wolfskin Lakewood Ride Sandal')]"));
+        webElement = driver.findElement(xpath("//*[contains(text(),'Сандалии Jack Wolfskin Lakewood Ride Sandal')]"));
         webElement.click();
+        webElement = driver.findElement(xpath("//*[@class='product-buttons__item ng-star-inserted']//*[@aria-label='Купить']"));
+        webElement.click();
+        webElement = driver.findElement(xpath("//*[contains(text(),' Оформить заказ ')]"));
+        webElement.click();
+        String price = driver.findElement(xpath("//*[@class='checkout-total__value checkout-total__value_size_large']")).getText();
+        assertEquals(price, expectedPrice);
     }
 
     @AfterMethod
